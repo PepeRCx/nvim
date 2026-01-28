84a8d3d7225073cc6f0a73ed5ddef1fa90a5c412
Pepe <josepp2508@gmail.com>
2026-01-28 16:39:54 -0600

    Initial config

---
+++ lua/configs/conform.lua
@@ -0,0 +1,19 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    javascript = { "eslint_d" },
+    typescript = { "eslint_d" },
+    javascriptreact = { "eslint_d" },
+    typescriptreact = { "eslint_d" }
+    -- css = { "prettier" },
+    -- html = { "prettier" },
+  },
+
+  format_on_save = {
+  --   -- These options will be passed to conform.format()
+     timeout_ms = 500,
+     lsp_fallback = true,
+  },
+}
+
+return options
