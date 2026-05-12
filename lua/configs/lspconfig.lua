2374e29f236fd835f190711aa158449a2d6401de
pepe <josepp2508@gmail.com>
2026-05-12 16:31:13 -0600

    [REM] formmat on save, already configured in conform.lua
    Signed-off-by: pepe <josepp2508@gmail.com>

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,15 +2,6 @@ require("nvchad.configs.lspconfig").defaults()
 
 local on_attach = function(client, bufnr)
   require("nvchad.configs.lspconfig").on_attach(client, bufnr)
-
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format({ async = false })
-      end,
-    })
-  end
 end
 
 local on_init = require("nvchad.configs.lspconfig").on_init
