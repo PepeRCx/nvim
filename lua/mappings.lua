0db84177b2e9c48094a58bed7aa18e3b7cda127c
pepe <josepp2508@gmail.com>
2026-05-12 17:42:08 -0600

    [FEAT] <leader> + 'e' now toggles between open/close tree
    Signed-off-by: pepe <josepp2508@gmail.com>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>e", "<cmd> NvimTreeToggle <cr>", { desc = "Toggle file explorer" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
